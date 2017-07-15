550983c2ab6e66f15b50ded6536aae78a1e1b569
Armen G <armen_ar@mail.ru>
2017-07-15 17:32:46 +0400

    installed git & nodejs & gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var jade = require('gulp-jade');
+ 
+gulp.task('templates', function() {
+  var YOUR_LOCALS = {};
+ 
+  gulp.src('./jade/*.jade')
+    .pipe(jade({
+    //   locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('./'))
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
